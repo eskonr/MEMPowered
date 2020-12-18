1eca73525b39defe35948726f1f641e71ff9fdce
Eswar Koneti (MVP) <eskonr@gmail.com>
2020-12-18 20:56:34 +0800

    Create Extract HashID for Autopilot.sql

---
+++ Reports/Autopilot/Extract HashID for Autopilot.sql
@@ -0,0 +1,12 @@
+select 
+pb.SerialNumber0 'Device Serial Number',
+os.SerialNumber0 'Windows Product ID',
+mdm.DeviceHardwareData0 'Hardware Hash',
+'Group Tag'='SCCM'
+from
+v_GS_PC_BIOS pb
+inner join v_R_System sys on sys.ResourceID=pb.ResourceID
+inner join v_GS_OPERATING_SYSTEM os on os.ResourceID=pb.ResourceID
+inner join v_GS_MDM_DEVDETAIL_EXT01 mdm on mdm.ResourceID=pb.ResourceID
+order by 1
+
