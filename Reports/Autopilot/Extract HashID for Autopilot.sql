a30cda66e36513af67c905b3a438d056624ec7e2
Eswar Koneti (MVP) <eskonr@gmail.com>
2020-12-18 20:59:40 +0800

    Update Extract HashID for Autopilot.sql

--- Reports/Autopilot/Extract HashID for Autopilot.sql
+++ Reports/Autopilot/Extract HashID for Autopilot.sql
@@ -8,5 +8,6 @@ v_GS_PC_BIOS pb
 inner join v_R_System sys on sys.ResourceID=pb.ResourceID
 inner join v_GS_OPERATING_SYSTEM os on os.ResourceID=pb.ResourceID
 inner join v_GS_MDM_DEVDETAIL_EXT01 mdm on mdm.ResourceID=pb.ResourceID
+where sys.Name0='Win10-001'
 order by 1
 
