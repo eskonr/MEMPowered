88cc93dff543e1cae3b27627678274f71c251cdc
Eswar Koneti (MVP) <eskonr@gmail.com>
2021-07-01 01:04:39 +0530

    scripts

---
+++ Scripts/Office365/AzureAD/Add-AAD-Devices to Group/Add-AADDevices-Group.ps1
@@ -0,0 +1,39 @@
+﻿<#
+Title:Add Azure AD join devices to AD group as alternative approach. There is limitation adding devices to group using dynamic query in portal.
+Author:Eswar Koneti
+Date:17-Aug-2019
+#>
+
+#Read-Host -Prompt "Enter your tenant password" -AsSecureString | ConvertFrom-SecureString | Out-File "C:\Temp\eswar\Scripts\Automation\eswaro365.key"
+
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+$date = (get-date -f dd-MM-yyyy-hhmmss)
+$inputfile = "$dir\AADDevices.txt"
+$Outfile = "$dir\o365licenseremoval-Direct-"+$date+".csv"
+$TenantUname = "eswar@xxxxx.com"
+$TenantPass = cat "C:\Temp\eswar\Scripts\Automation\eswaro365.key" | ConvertTo-SecureString
+$TenantCredentials = new-object -typename System.Management.Automation.PSCredential -argumentlist $TenantUname, $TenantPass
+
+#connect to AzureAD module
+Connect-AzureAD #-Credential $TenantCredentials
+
+#get a list of all device members which already in group AzureADJoin:
+#f9eaed44-f34a-4931-b8e0-a41f7f8454ba=All AAD Devices
+$members=Get-AzureADGroupMember -ObjectId "f9eaed44-f34a-4931-b8e0-a41f7f8454ba" | Where-Object {$_.ObjectType -eq "Device"}
+
+#get a list of all Azure AD joined devices:
+$devices=Get-AzureADDevice -All $true | Where-Object {$_.DeviceTrustType -eq "AzureAd"}
+if ($Devices)
+{ 
+      foreach ($device in $devices)
+      {
+        #Check if the device is already a member of group AzureADjoin.
+        #if not, add it to the group
+             if ($members.ObjectId -notcontains $device.ObjectId)
+             {
+            
+                   Add-AzureADGroupMember -ObjectId "f9eaed44-f34a-4931-b8e0-a41f7f8454ba" -RefObjectId $device.ObjectId
+             }
+      }
+} 
---
+++ Scripts/Office365/AzureAD/Get-MAMDevices/Get-AzureDevicesv1.0.ps1
@@ -0,0 +1,55 @@
+﻿
+<#
+Title:Get list of All Azure AD devices (registered and hybrid azure AD join and enrolled)
+Author:Eswar Koneti
+Date:26-Aug-2018
+#>
+
+
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+$date = (get-date -f dd-MM-yyyy-hhmmss)
+$Outfile = "$dir\AzureADDevices-"+$date+".csv"
+Connect-AzureAD
+
+$aadDevices = Get-AzureADDevice -All $true | Where-Object {$_.DeviceOSType -notlike "Windows*"}
+
+$aadDevices | Get-Member
+
+$aadDevices.count
+
+$DeviceInfo = @()
+
+ForEach ($aadDevice in $aadDevices) {
+
+    $device = New-Object -TypeName PSObject
+   $device | Add-Member -Type NoteProperty -Name ObjectId -Value $aadDevice.ObjectId
+
+    $device | Add-Member -Type NoteProperty -Name DeviceOSType -Value $aadDevice.DeviceOSType
+    $device | Add-Member -Type NoteProperty -Name DeviceOSVersion -Value $aadDevice.DeviceOSVersion
+   
+    $device | Add-Member -Type NoteProperty -Name DisplayName -Value $aadDevice.DisplayName
+    $device | Add-Member -Type NoteProperty -Name IsCompliant -Value $aadDevice.IsCompliant
+    $device | Add-Member -Type NoteProperty -Name IsManaged -Value $aadDevice.IsManaged
+   
+    If ($aadDevice.ApproximateLastLogonTimeStamp) {$device | Add-Member -Type NoteProperty -Name ApproximateLastLogonTimeStamp -Value ([datetime]$aadDevice.ApproximateLastLogonTimeStamp) }
+        $deviceOwner = Get-AzureADDeviceRegisteredOwner -ObjectId $aadDevice.ObjectId
+    If ($deviceOwner) {     #   Write-Host $aadDevice.DisplayName $deviceOwner.DisplayName -ForegroundColor Yellow
+  
+     $device | Add-Member -Type NoteProperty -Name OwnerUserPrincipalName -Value $deviceOwner.UserPrincipalName
+    }
+
+    $deviceUser = Get-AzureADDeviceRegisteredUser -ObjectId $aadDevice.ObjectId
+    If ($deviceUser) {
+   
+        $device | Add-Member -Type NoteProperty -Name RegisteredUserUserPrincipalName -Value $deviceUser.UserPrincipalName
+    }
+
+    $DeviceInfo += $device
+
+}
+
+$DeviceInfo | Select ObjectId,DeviceOSType,DeviceOSVersion,DisplayName,IsCompliant,IsManaged,ApproximateLastLogonTimeStamp,OwnerUserPrincipalName,RegisteredUserUserPrincipalName | Export-Csv $Outfile -Encoding UTF8 -NoTypeInformation -Delimiter ";"
+
+
+#>
