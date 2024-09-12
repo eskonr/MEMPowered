6ccbc5e7a6807dc255c43d52e9e6008949fc4e89
Eswar Koneti (MVP) <eskonr@gmail.com>
2024-09-12 23:21:06 +0800

    Add files via upload

---
+++ Scripts/Intune/Requirement rules/RequirementRule-IsHybridDomainJoined.ps1
@@ -0,0 +1,38 @@
+﻿<#
+.SYNOPSIS
+This script checks if the computer is hybrid Azure AD joined before proceeding with application installation in Intune.
+
+.DESCRIPTION
+The script uses `dsregcmd /status` to determine if the device is both Azure AD joined and Domain joined. If both conditions are met, the script outputs `1` to indicate that the requirement is satisfied.
+
+#>
+
+# Ensure no PowerShell errors are captured in the error stream
+$ErrorActionPreference = "SilentlyContinue"
+
+# Initialize requirement flag
+$b_Required = $False
+
+function Get-DsRegStatus {
+    $dsregcmd = dsregcmd /status
+    $status = $dsregcmd | Select-String -Pattern " *[A-z]+ : .+ *"
+    $result = @{}
+    foreach ($line in $status) {
+        $parts = ($line.ToString().Trim() -split " : ")
+        $result[$parts[0]] = $parts[1]
+    }
+    return $result
+}
+
+# Check if device is Azure AD joined and Domain joined
+$status = Get-DsRegStatus
+if ($status["AzureADJoined"] -eq "YES" -and $status["DomainJoined"] -eq "YES") {
+    $b_Required = $True
+}
+
+# Output the result
+if ($b_Required) { Write-Output 1 }
+
+# Clear errors and exit
+$Error.Clear()
+exit 0
--- Scripts/Intune/Requirement rules/RequirementRule-IsMultiSessionOS.ps1
+++ Scripts/Intune/Requirement rules/RequirementRule-IsMultiSessionOS.ps1
@@ -5,6 +5,7 @@ This script checks if the operating system is a multi-session OS before proceedi
 The script queries the Win32_OperatingSystem class to determine if the OS is a multi-session version. If the OS caption includes the term "Multi-Session",
  it indicates that the OS supports multiple user sessions, and the script exits with code 1. If the OS is not a multi-session version, the script exits with code 0.
 File Name: RequirementRule-IsMultiSessionOS.ps1
+Author:Eswar Koneti
 #>
 
 # Query the Win32_OperatingSystem class
