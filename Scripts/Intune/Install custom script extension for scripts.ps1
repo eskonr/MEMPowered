1832e3b7ec41e03361b958462678610959f841a2
Eswar Koneti (MVP) <eskonr@gmail.com>
2024-09-26 20:39:15 +0800

    Add files via upload

---
+++ Scripts/Intune/Install custom script extension for scripts.ps1
@@ -0,0 +1,18 @@
+﻿$fileUri = @("https://cb1cmg02.blob.core.windows.net/scripts/VMextention.ps1")
+
+$settings = @{"fileUris" = $fileUri};
+
+$storageAcctName = "cb1cmg02"
+$storageKey = "12345"
+$protectedSettings = @{"storageAccountName" = $storageAcctName; "storageAccountKey" = $storageKey; "commandToExecute" = "powershell -ExecutionPolicy Unrestricted -File VMextention.ps1"};
+
+#run command
+New-AzConnectedMachineExtension -ResourceGroupName "Intune-svc-logs" `
+    -Location "Southeast Asia" `
+    -MachineName "AZ-SRV-003" `
+    -Name "TestInstall" `
+    -Publisher "Microsoft.Compute" `
+    -ExtensionType "CustomScriptExtension" `
+    -TypeHandlerVersion "1.10" `
+    -Settings $settings `
+    -ProtectedSettings $protectedSettings;
