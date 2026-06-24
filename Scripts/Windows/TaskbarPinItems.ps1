09bd28c62716d06945f7e64ff238d88babc18df3
Eswar Koneti (MVP) <eskonr@gmail.com>
2026-06-24 20:39:41 +0800

    Add files via upload

---
+++ Scripts/Windows/TaskbarPinItems.ps1
@@ -0,0 +1,51 @@
+﻿
+<#
+.SYNOPSIS
+Configures a default taskbar layout for new Windows user profiles.
+
+.DESCRIPTION
+This script creates a LayoutModification.xml file in the Default user profile directory.
+The XML defines a custom taskbar layout that replaces existing pinned applications
+with a standardized set.
+
+Specifically, it pins:
+- File Explorer (Microsoft.Windows.Explorer)
+- Microsoft Edge (MSEdge)
+
+The configuration applies only to newly created user profiles and does not modify
+existing users.
+
+.PARAMETER None
+This script does not accept parameters.
+
+.NOTES
+- Must be run with appropriate permissions to write to the Default profile.
+- Typically used in imaging, provisioning, or enterprise deployment scenarios.
+
+Author: Eswar Koneti
+Date: 16-May-2026
+#>
+
+
+$taskbarFile = "$env:SystemDrive\Users\Default\AppData\Local\Microsoft\Windows\Shell\LayoutModification.xml"
+[xml]$taskbarXml = @"
+<?xml version="1.0" encoding="utf-8"?>
+<LayoutModificationTemplate
+    xmlns="http://schemas.microsoft.com/Start/2014/LayoutModification"
+    xmlns:defaultlayout="http://schemas.microsoft.com/Start/2014/FullDefaultLayout"
+    xmlns:start="http://schemas.microsoft.com/Start/2014/StartLayout"
+    xmlns:taskbar="http://schemas.microsoft.com/Start/2014/TaskbarLayout"
+    Version="1">
+  <CustomTaskbarLayoutCollection PinListPlacement="Replace">
+    <defaultlayout:TaskbarLayout>
+      <taskbar:TaskbarPinList>
+        <taskbar:DesktopApp DesktopApplicationID="Microsoft.Windows.Explorer" />
+        <taskbar:DesktopApp DesktopApplicationID="MSEdge"/>
+        </taskbar:TaskbarPinList>
+    </defaultlayout:TaskbarLayout>
+ </CustomTaskbarLayoutCollection>
+</LayoutModificationTemplate>
+"@
+if (-NOT[string]::IsNullOrEmpty($taskbarXml)) {
+    $taskbarXml.Save($taskbarFile)
+}
---
+++ Scripts/Windows/TaskbarPinItemsANDCustomisations.ps1
@@ -0,0 +1,75 @@
+﻿
+<#
+.SYNOPSIS
+    Configure Windows 11 taskbar layout and search behavior and
+    Configures Windows 11 taskbar search experience to show icon-only view.
+
+.DESCRIPTION
+    This script sets registry values to enforce the search box on the taskbar 
+    to display as an icon (instead of the full search box) in Windows 11.
+
+    It applies the configuration at both:
+        - Machine level (HKLM)
+        - Default user profile (HKU\Default)
+
+    It also disables Search Highlights to prevent Windows from overriding 
+    the configured search behavior.
+
+    This ensures that all new user profiles receive a consistent taskbar 
+    configuration during SCCM / OSD deployment.
+
+.AUTHOR
+    Eswar Koneti
+
+.DATE
+    19-May-2026
+
+.VERSION
+    1.0
+
+.NOTES
+    - Intended for use in SCCM / MECM Task Sequences
+    - Must be executed after "Setup Windows and ConfigMgr"
+    - Requires administrative privileges
+    - Applies only to new user profiles (existing users are not modified)
+
+.EXAMPLE
+    Run as part of a Task Sequence to standardize Windows 11 taskbar layout
+    with search icon only and no search highlights.
+#>
+
+# Configure Taskbar Layout (Default User)
+$taskbarFile = "$env:SystemDrive\Users\Default\AppData\Local\Microsoft\Windows\Shell\LayoutModification.xml"
+[xml]$taskbarXml = @"
+<?xml version="1.0" encoding="utf-8"?>
+<LayoutModificationTemplate
+    xmlns="http://schemas.microsoft.com/Start/2014/LayoutModification"
+    xmlns:defaultlayout="http://schemas.microsoft.com/Start/2014/FullDefaultLayout"
+    xmlns:start="http://schemas.microsoft.com/Start/2014/StartLayout"
+    xmlns:taskbar="http://schemas.microsoft.com/Start/2014/TaskbarLayout"
+    Version="1">
+  <CustomTaskbarLayoutCollection PinListPlacement="Replace">
+    <defaultlayout:TaskbarLayout>
+      <taskbar:TaskbarPinList>
+        <taskbar:DesktopApp DesktopApplicationID="Microsoft.Windows.Explorer" />
+        <taskbar:DesktopApp DesktopApplicationID="MSEdge"/>
+        </taskbar:TaskbarPinList>
+    </defaultlayout:TaskbarLayout>
+ </CustomTaskbarLayoutCollection>
+</LayoutModificationTemplate>
+"@
+if (-NOT[string]::IsNullOrEmpty($taskbarXml)) {
+    $taskbarXml.Save($taskbarFile)
+}
+
+# Configure Search (Machine Level)
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /v SearchboxTaskbarMode /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /v SearchboxTaskbarModeCache /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v EnableDynamicContentInWSB /t REG_DWORD /d 0 /f
+
+#Configure Default User Profile
+reg load HKU\Default C:\Users\Default\NTUSER.DAT
+reg add "HKU\Default\Software\Microsoft\Windows\CurrentVersion\Search" /v SearchboxTaskbarMode /t REG_DWORD /d 1 /f
+reg add "HKU\Default\Software\Microsoft\Windows\CurrentVersion\Search" /v SearchboxTaskbarModeCache /t REG_DWORD /d 1 /f
+reg unload HKU\Default
+
